24c4fa0a70e244849c90fbbc5afebe49a39199f1
alin <alinnurkhalifah@gmail.com>
2017-06-06 13:45:10 +0700

    database

---
+++ penyakit-unggas.sql
@@ -0,0 +1,140 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 06 Jun 2017 pada 08.44
+-- Versi Server: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penyakit-unggas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `gejala`
+--
+
+CREATE TABLE `gejala` (
+  `id` int(11) NOT NULL,
+  `kode` varchar(5) NOT NULL,
+  `keterangan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `gejala`
+--
+
+INSERT INTO `gejala` (`id`, `kode`, `keterangan`) VALUES
+(4, 'G0001', 'Ngorok'),
+(5, 'G0002', 'Kotoran berwarna hijau');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `gejala_penyakit`
+--
+
+CREATE TABLE `gejala_penyakit` (
+  `id` int(11) NOT NULL,
+  `gejala_id` int(11) NOT NULL,
+  `penyakit_id` int(11) NOT NULL,
+  `md` float NOT NULL,
+  `mb` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `gejala_penyakit`
+--
+
+INSERT INTO `gejala_penyakit` (`id`, `gejala_id`, `penyakit_id`, `md`, `mb`) VALUES
+(2, 4, 2, 0.1, 0.9);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penyakit`
+--
+
+CREATE TABLE `penyakit` (
+  `id` int(11) NOT NULL,
+  `kode` varchar(5) NOT NULL,
+  `keterangan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `penyakit`
+--
+
+INSERT INTO `penyakit` (`id`, `kode`, `keterangan`) VALUES
+(2, 'P0001', 'Tetelo'),
+(3, 'P0002', 'Corella');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gejala`
+--
+ALTER TABLE `gejala`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `gejala_penyakit`
+--
+ALTER TABLE `gejala_penyakit`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `gejala_id` (`gejala_id`),
+  ADD KEY `penyakit_id` (`penyakit_id`);
+
+--
+-- Indexes for table `penyakit`
+--
+ALTER TABLE `penyakit`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gejala`
+--
+ALTER TABLE `gejala`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `gejala_penyakit`
+--
+ALTER TABLE `gejala_penyakit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `penyakit`
+--
+ALTER TABLE `penyakit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `gejala_penyakit`
+--
+ALTER TABLE `gejala_penyakit`
+  ADD CONSTRAINT `gejala_penyakit_ibfk_1` FOREIGN KEY (`penyakit_id`) REFERENCES `penyakit` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `gejala_penyakit_ibfk_2` FOREIGN KEY (`gejala_id`) REFERENCES `gejala` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
